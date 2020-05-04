0b3de1ed165d03c5f2263823456bdfa941e2cdb9
cathyp1612 <61745818+cathyp1612@users.noreply.github.com>
2020-05-04 20:06:39 +0000

    done with day 1

--- ShoppingApi/Startup.cs
+++ ShoppingApi/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ShoppingApi.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace ShoppingApi
 {
@@ -25,6 +27,12 @@ namespace ShoppingApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ShoppingDataContext>(options =>
+             {
+                options.UseSqlServer(Configuration.GetConnectionString("shopping"));
+
+             });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
