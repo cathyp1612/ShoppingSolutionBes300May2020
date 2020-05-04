67257ade7882db0292de10507174dffb1f83f95a
cathyp1612 <61745818+cathyp1612@users.noreply.github.com>
2020-05-04 16:51:28 +0000

    Add project files.

---
+++ ShoppingApi/Models/GetShoppingListResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingApi.Models
+{
+    public class GetShoppingListResponse
+    {
+        public List<ShoppingLIstItemResponse> Data { get; set; }
+    }
+    public class ShoppingLIstItemResponse
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool Purchased { get; set; }
+    }
+}
